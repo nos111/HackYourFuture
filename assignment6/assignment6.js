6bf82a85f454383c16edb9a23399743c76d29a85
nos111 <nsaffour@gmail.com>
2017-05-24 12:51:32 +0200

    Completed assignment 6

--- assignment6/assignment6.js
+++ assignment6/assignment6.js
@@ -1,65 +1,129 @@
 var info;
-document.getElementById("user-name-button").addEventListener('click',page);
+var userName = 'nos111';
+var reposInfo;
+var hoverEvent;
 
-function page (){
+document.getElementById("user-name-button").addEventListener('click',initSearch);
+
+function initSearch (){
     document.getElementById("info").innerHTML = "";
-    var username = userNameCatcher();
-    dataRequest(username,processData);
-    
-    
+    var link = inputCatcher();
+    dataRequest(link,processData);    
 }
-function dataRequest(searchTerm,callBack) { //Make a data request to the API
-    var request = new XMLHttpRequest;
+
+function init (){
+    var link = 'https://api.github.com/users/nos111';
+    dataRequest(link,processData);    
+}
+
+function dataRequest(link,callBack) { //Make a data request to the API
+    var request = new XMLHttpRequest();
     request.onreadystatechange = function(){
         if(request.readyState === 4) {
             if(typeof callBack == 'function')
-            callBack(request.responseText);
-            console.log("searching")
+                callBack(request.responseText);
         }
     };
-    request.open('GET',searchTerm);
+    request.open('GET',link);
     request.send();
 }
 
-function userNameCatcher () { //catches username 
-    var userName = 'https://api.github.com/users/' + document.getElementById("user-name").value;
-    if (userName == " " || userName == "/" || userName=="?"){
-         document.getElementById("info").innerHTML = "";
+//catches username and check for good user input
+function inputCatcher () {  
+    userName = document.getElementById("user-name").value;
+    if (userName === " " || userName === "/" || userName ==="?" || userName ===''){
+        document.getElementById("info").innerHTML = "";
         document.getElementById("warning-message").innerHTML = "Please enter a valid search term";
-    }else if (userName === 'https://api.github.com/users/'){ //to make the page go to my repository on start
-        userName = 'https://api.github.com/users/nos111'
-        return userName;
     }else{
-        return userName;
-    }
-    
+        return 'https://api.github.com/users/' + userName;
+    } 
 }
 
-function processData (asd) { //processes the returned data from the request
-         //When the request is done this will be activated
-            var dataFile = JSON.parse(asd);
-            console.log(dataFile.url);
+//Processes the returned data from the request
+//When the request is done this function will be activated
+function processData (response) { 
+            var dataFile = JSON.parse(response);
             if (dataFile.message == "Not Found") {
                 document.getElementById("info").innerHTML = "";
                 document.getElementById("warning-message").innerHTML = "Please enter a valid user name";
         } else {
             document.getElementById("warning-message").innerHTML = "";
-            createList(dataFile);
+            createProfile(dataFile);
             }
     }
 
-function createList (data) {//creates html elements
+//Creates profile html elements
+function createProfile (data) {
     info = data;
-    createElement("h1","info",'<a href="#">'+data.name+" profile","id","name").addEventListener('click',nameClick);
-    createElement("h3","info",'<a href="#">'+"Number of public repos is "+data.public_repos,"id","repos");
+    createElement("section","info",'<a href="#">'+data.name+" profile","id","name").addEventListener('click',nameClick);
+    createElement("section","info",'<a href="#">'+"Number of public repos is "+data.public_repos,"id","repos");
     createElement("a","info",'<img src="'+data.avatar_url+'">',"href",data.html_url,"target","_blank");
+    document.getElementById("repos").addEventListener('click',reposInit);
 }
 
-function nameClick(){ //creates extra info elements when clicked on the name
-    if (document.getElementById("memeber-since") == null) {
-    createElement("p","name","Git member since"+info.created_at,"id","memeber-since");
-    createElement('p','name','Git ID:'+info.id);
+//creates extra info elements when clicked on the name
+function nameClick(){ 
+    if (document.getElementById("memeber-since") === null) { //Make sure the list is loaded only once
+        createElement("p","name","Git member since "+info.created_at,"id","memeber-since");
+        createElement('p','name','Git ID:'+info.id);
     }
 }
 
-page();
+//Start building repos section
+function reposInit(){
+    var link = 'https://api.github.com/users/'+ userName + '/repos';
+    if (document.getElementById('repos-list') === null) //Make sure the list is only created once
+        dataRequest(link,createReposElements);
+}
+
+//Initiate repos
+function createReposElements(data) {
+    reposInfo = JSON.parse(data);
+    if(reposInfo[0] !== undefined) {
+        createElement('ul','repos',undefined,'id','repos-list');
+        reposInfo.forEach( function(object,i){
+            createElement('li','repos-list',object.name,'id',i,'class','repo').addEventListener('mouseover',reposHover);
+        });
+    }
+}
+
+
+//Create HTML elements on hover over repos
+function reposHover(mouseHover){
+    hoverEvent = mouseHover;
+    document.getElementById(hoverEvent.target.id).removeEventListener('mouseover',reposHover);
+    //make sure we have got data back from the request
+    if (reposInfo[parseInt(mouseHover.target.id)] !== undefined) { 
+        createElement('aside',mouseHover.target.id,reposInfo[parseInt(mouseHover.target.id)].description,'class','repos-description');
+        createElement('aside',mouseHover.target.id,'Created at ' + reposInfo[parseInt(mouseHover.target.id)].created_at,'class','repos-description');
+        createElement('aside',mouseHover.target.id,'Number of open issues ' + reposInfo[parseInt(mouseHover.target.id)].created_at,'class','repos-description');
+        requestCollaboratorsInfo();
+    }
+    
+    
+}
+
+//Make APIT call to get collaborators info
+function requestCollaboratorsInfo () {
+    var link = 'https://api.github.com/repos/' + userName + '/' + reposInfo[parseInt(hoverEvent.target.id)].name +'/events';
+    dataRequest(link,collaboratorsRequest);
+}
+
+//Request extra data when hovered over repo
+function collaboratorsRequest(data) {
+    var collaboratorsInfo = JSON.parse(data);
+    //Make sure we have got data 
+    if(collaboratorsInfo[0] !== undefined){
+        createElement('p',hoverEvent.target.id,'Latest event:');
+        collaboratorsInfo.forEach(function(object,index){
+            createElement('p',hoverEvent.target.id,'Type of evevnt: ' + object.type,'id',index+'collaborator');
+            if (object.type == "PushEvent") {
+            createElement('p',hoverEvent.target.id,'Committ: ' + object.payload.commits[0].message);
+            }
+        });
+    }
+    
+}
+
+
+init();
